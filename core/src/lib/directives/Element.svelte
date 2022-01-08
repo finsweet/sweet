d56e4a9d5b1b386c61fb7964b74a031c112379aa
Alex Iglesias <alexiglesias@finsweet.com>
2022-01-08 17:08:09 +0000

    Refactored `Element.svelte`

--- core/src/lib/directives/Element.svelte
+++ core/src/lib/directives/Element.svelte
@@ -1,7 +1,7 @@
 <script lang="ts" context="module">
   import { CLOAK_ATTRIBUTE, ELEMENT_CONTEXT, ELEMENT_ATTRIBUTE } from '../utils/constants';
 
-  let renderedElements: { e: Element; o?: Element }[] = [];
+  let renderedElements: { e: Element; o: Element }[] = [];
 
   const elements = document.querySelectorAll(`[${ELEMENT_ATTRIBUTE}]`);
 
@@ -16,14 +16,17 @@
   export let selector: string | undefined = undefined;
   export let element: Element | undefined = undefined;
 
+  let error: string | undefined;
+
   const currentContext = getContext<Element>(ELEMENT_CONTEXT) || document;
   const originalElement = element || currentContext.querySelector(selector || `[${ELEMENT_ATTRIBUTE}='${name}']`);
 
   const existingElements = renderedElements.filter(({ o }) => o === originalElement);
   const existingElementsCount = existingElements.length;
 
-  let error: string | undefined;
-
+  /**
+   * Assign the element.
+   */
   if (!existingElementsCount) element = originalElement;
   else {
     element = originalElement.cloneNode(true) as Element;
@@ -35,8 +38,14 @@
 
   renderedElements.push({ e: element, o: originalElement });
 
+  /**
+   * Init context.
+   */
   setContext(ELEMENT_CONTEXT, element);
 
+  /**
+   * Make sure the element is visible.
+   */
   onMount(() => {
     try {
       element.removeAttribute(CLOAK_ATTRIBUTE);
@@ -45,6 +54,9 @@
     }
   });
 
+  /**
+   * Make sure the element is hidden/removed.
+   */
   onDestroy(() => {
     if (existingElementsCount) element.remove();
     else element.setAttribute(CLOAK_ATTRIBUTE, '');
